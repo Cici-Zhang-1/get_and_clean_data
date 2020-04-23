b520860f41bd0390ed580c8f1a98f88d7218277f
mrzhangcc <cczhang1987@hotmail.com>
2020-04-22 21:03:41 -0400

    add the source code

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # get_and_clean_data
+
+The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis. 
+
+# Project contains several files
+
+## run_analysis.R
+
+This code complete the following steps:
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names.
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## merge_data.txt
+
+merge_data.txt stored the 4th step data which contains the test and train data together.
+
+## tidy.txt
+
+tidy.txt stored the tidy data which contains the average of each variable for each activity and each subject
---
+++ run_analysis.R
@@ -0,0 +1,68 @@
+## Getting and cleaning data course project
+## The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set.
+## 
+library(dplyr)
+library(stringr)
+library(tidyr)
+
+## set up default folders
+dataFolder = "Dataset"
+testFolder = file.path(dataFolder, "test")
+trainFolder = file.path(dataFolder, 'train')
+
+rootFolder = getwd()
+setwd(file.path(rootFolder, dataFolder))
+## read features
+features <- read.table('./features.txt', header = FALSE, col.names = c('fid', 'fname')) %>%
+  tbl_df() %>%
+  mutate(fname = gsub('\\(|\\)|,|-|\\.', '_', fname)) %>%
+  mutate(fname = gsub('_+', '_', fname))
+getwd()
+## read activity labels
+activityLabels <- read.table('./activity_labels.txt', header = FALSE, col.names = c('aid', 'aname')) %>%
+  tbl_df()
+
+
+## read test data, combine subject, x test, y test together
+setwd(file.path(rootFolder, testFolder))
+subjectTest <- read.table('./subject_test.txt', header = FALSE, col.names = c('subject'), colClasses = 'factor')
+yTest <- read.table('./y_test.txt', header = FALSE, col.names = c('activity'), colClasses = 'factor') %>%
+  tbl_df() %>% 
+  merge(activityLabels, by.x = 'activity', by.y = 'aid') %>%
+  select(aname)%>%
+  rename(activity = aname)
+xTest <- read.table('./X_test.txt', header = FALSE, col.names = features$fname) %>%
+  tbl_df() %>% 
+  select(contains(c('mean', 'std')))
+subjectTest <- cbind(subjectTest, yTest, xTest) %>% tbl_df()
+
+## read train data, combine subject, xtest, y test together
+setwd(file.path(rootFolder, trainFolder))
+subjectTrain <- read.table('./subject_train.txt', header = FALSE, col.names = c('subject'), colClasses = 'factor')
+yTrain <- read.table('./y_train.txt', header = FALSE, col.names = c('activity')) %>%
+  tbl_df() %>% 
+  merge(activityLabels, by.x = 'activity', by.y = 'aid') %>% 
+  select(aname) %>%
+  rename(activity = aname)
+xTrain <- read.table('./X_train.txt', header = FALSE, col.names = features$fname) %>%
+  tbl_df() %>% select(contains(c('mean', 'std')))
+subjectTrain <- cbind(subjectTrain, yTrain, xTrain) %>% tbl_df()
+
+## combine test and train data together
+subjectMerge <- rbind(subjectTest, subjectTrain)
+
+## create data set with the average of each variable for each activity and each subject
+secondSubjectMerge <- subjectMerge %>%
+  group_by(subject, activity) %>%
+  summarize_each(list(mean = mean))
+
+## return to the root folder
+setwd(rootFolder)
+
+## write merge data to merge_data.txt
+write.table(subjectMerge, 'merge_data.txt')
+
+## write tidy data to tidy file
+write.table(secondSubjectMerge, 'tidy.txt')
+
+
