fd7a17f395649afdc57d3bd12dfa0507a9028dcf
mrzhangcc <cczhang1987@hotmail.com>
2020-04-22 22:54:43 -0400

    repair missing data

--- run_analysis.R
+++ run_analysis.R
@@ -25,28 +25,27 @@ activityLabels <- read.table('./activity_labels.txt', header = FALSE, col.names 
 
 ## read test data, combine subject, x test, y test together
 setwd(file.path(rootFolder, testFolder))
-subjectTest <- read.table('./subject_test.txt', header = FALSE, col.names = c('subject'), colClasses = 'factor')
-yTest <- read.table('./y_test.txt', header = FALSE, col.names = c('activity'), colClasses = 'factor') %>%
-  tbl_df() %>% 
-  merge(activityLabels, by.x = 'activity', by.y = 'aid') %>%
-  select(aname)%>%
-  rename(activity = aname)
-xTest <- read.table('./X_test.txt', header = FALSE, col.names = features$fname) %>%
-  tbl_df() %>% 
-  select(contains(c('mean', 'std')))
-subjectTest <- cbind(subjectTest, yTest, xTest) %>% tbl_df()
+subjectTest <- read.table('./subject_test.txt', header = FALSE, col.names = c('subject'))
+yTest <- read.table('./y_test.txt', header = FALSE, col.names = c('activity')) 
+xTest <- read.table('./X_test.txt', header = FALSE, col.names = features$fname)
+subjectTest <- cbind(subjectTest, yTest, xTest) %>%
+  tbl_df() %>%
+  merge(activityLabels, by.x = 'activity', by.y = 'aid', sort = FALSE) %>%
+  select(-activity) %>%
+  rename(activity = aname) %>%
+  select(contains(c('subject', 'activity', '_mean_', '_std_'), ignore.case = FALSE))
 
 ## read train data, combine subject, xtest, y test together
 setwd(file.path(rootFolder, trainFolder))
-subjectTrain <- read.table('./subject_train.txt', header = FALSE, col.names = c('subject'), colClasses = 'factor')
-yTrain <- read.table('./y_train.txt', header = FALSE, col.names = c('activity')) %>%
-  tbl_df() %>% 
-  merge(activityLabels, by.x = 'activity', by.y = 'aid') %>% 
-  select(aname) %>%
-  rename(activity = aname)
-xTrain <- read.table('./X_train.txt', header = FALSE, col.names = features$fname) %>%
-  tbl_df() %>% select(contains(c('mean', 'std')))
-subjectTrain <- cbind(subjectTrain, yTrain, xTrain) %>% tbl_df()
+subjectTrain <- read.table('./subject_train.txt', header = FALSE, col.names = c('subject'))
+yTrain <- read.table('./y_train.txt', header = FALSE, col.names = c('activity'))
+xTrain <- read.table('./X_train.txt', header = FALSE, col.names = features$fname)
+subjectTrain <- cbind(subjectTrain, yTrain, xTrain) %>% 
+  tbl_df() %>%
+  merge(activityLabels, by.x = 'activity', by.y = 'aid', sort = FALSE) %>%
+  select(-activity) %>%
+  rename(activity = aname) %>%
+  select(contains(c('subject', 'activity', '_mean_', '_std_'), ignore.case = FALSE))
 
 ## combine test and train data together
 subjectMerge <- rbind(subjectTest, subjectTrain)
